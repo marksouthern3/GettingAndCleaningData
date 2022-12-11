906836186b5da467f527765ade97bbb79d2382e5
Mark Southern <marksouthern3@gmail.com>
2022-12-11 12:37:39 +0000

    Extract mean and std

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,11 @@
 features <- read.table("UCI HAR Dataset/features.txt")[, 2]
+meanandstd <- grep("mean|std", features)
 activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")[, 2]
 
 # load test dataset
 X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
 colnames(X_test) <- features
+X_test <- X_test[, meanandstd]  # extract only mean and std
 y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
 colnames(y_test) <- "Activity"
 y_test$Activity <- as.factor(y_test$Activity)
@@ -15,6 +17,7 @@ test <- cbind(subject_test, y_test, X_test)
 # load training dataset
 X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
 colnames(X_train) <- features
+X_train <- X_train[, meanandstd]  # extract only mean and std
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
 colnames(y_train) <- "Activity"
 y_train$Activity <- as.factor(y_train$Activity)
